2243dec13bcc40d0cbdf5e5f9fe887a26f3b1e98
Elia Rodriguez <elle0586@mycodergv.org>
2016-10-25 21:50:51 -0500

    Commiting more files

--- guestbook/routes/index.js
+++ guestbook/routes/index.js
@@ -1,21 +1,62 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Guestbook' });
-});
-
 
 
 /* GET Userlist page. */
-router.get('/userlist', function(req, res) {
+router.get('/', function(req, res) {
     var db = req.db;
     var collection = db.get('usercollection');
     collection.find({},{},function(e,docs){
-        res.render('userlist', {
+        res.render('index', {
             "userlist" : docs
         });
     });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+
+/* POST to Add User Service */
+router.post('/adduser', function(req,res){
+	//set our internal db variable
+	var db = req.db;
+
+	//get our form values, these rely on the "name attributes"
+	var userName = req.body.username;
+	var userEmail = req.body.useremail;
+	var userMessage = req.body.usermessage;
+
+	//set our collection
+	var collection = db.get('usercollection');
+
+	//submit to the database
+	collection.insert({
+		"username" :userName,
+		"email": userEmail,
+		"message": userMessage
+
+	}, function(err,doc){
+		if(err){
+			//if it failed, return error
+			res.send("There was a problem adding the information to the database");
+		}
+		else{
+			// forward to success page
+			res.redirect("/");
+		}
+	});
+});
+// Remove guest from guestbook
+router.get('/:id/delete/', function (req, res) {
+  var id = req.params.id;
+  var objectId = new ObjectID(id);
+  var db = req.db;
+  var collection = db.get('guests');
+  collection.remove({"_id": objectId});
+  res.redirect('/');
 });
 module.exports = router;
